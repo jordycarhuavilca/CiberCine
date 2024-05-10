0775b79f5bca3e7bf1d8072e2747965175240053
GAD <josuealva920@gmail.com>
2024-05-10 16:19:22 -0500

    Implementacion Spring Security

--- src/main/java/pe/org/group02/ventaboletoscine/repository/UsuariosRepository.java
+++ src/main/java/pe/org/group02/ventaboletoscine/repository/UsuariosRepository.java
@@ -4,4 +4,5 @@ import org.springframework.data.repository.CrudRepository;
 import pe.org.group02.ventaboletoscine.entity.Usuarios;
 
 public interface UsuariosRepository extends CrudRepository<Usuarios, Integer> {
+    Usuarios findByUsuario(String usuario);
 }
